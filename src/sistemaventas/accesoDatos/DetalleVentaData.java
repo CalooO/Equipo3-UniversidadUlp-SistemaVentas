521c717a4441fee025ccf62d731310f896bef799
BenziGonzalo <141678583+BenziGonzalo@users.noreply.github.com>
2023-10-04 13:18:37 -0300

    g

--- src/sistemaventas/accesoDatos/DetalleVentaData.java
+++ src/sistemaventas/accesoDatos/DetalleVentaData.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
+import sistemaventas.entidades.Cliente;
 import sistemaventas.entidades.DetalleVenta;
 import sistemaventas.entidades.Producto;
 import sistemaventas.entidades.Venta;
@@ -17,10 +18,17 @@ public class DetalleVentaData {
     private Connection con = null;
     private Venta venta = new Venta();
     private Producto producto = new Producto();
+    private Cliente cliente = new Cliente();
+    private ProductoData prodData = new ProductoData();
+    private VentaData ventaData = new VentaData();
+    private ClienteData clienteData = new ClienteData();
     
     public List<DetalleVenta> detalleDeVentas(){
         
-        String sql = "select idDetalleVenta, cantidad, idVenta, precioVenta, idProducto from detalleventa";
+        String sql = "SELECT idDetalleVenta,idVenta,idProducto, nombreProducto, cantidad, precioVenta, fechaVenta"
+                + " FROM producto p join detalleventa d ON(p.idProducto = d.idProducto) "
+                + "JOIN venta v ON(d.idVenta = v.idVenta) "
+                + "WHERE idDetalleVenta = ?";
         
         ArrayList<DetalleVenta> listaDetalleVenta = new ArrayList<>();
         
@@ -37,7 +45,24 @@ public class DetalleVentaData {
                 detalleVenta.setIdDetalleVenta(rs.getInt("idDetalleVenta"));
                 detalleVenta.setCantidad(rs.getInt("cantidad"));
                 detalleVenta.setPrecioVenta(rs.getDouble("precioVenta"));
+                for(Producto producto:prodData.listarProducto()){
+                    
+                    if(rs.getInt("idProducto") == producto.getIdProducto())
+                        detalleVenta.setProducto(producto);
+                    if(rs.getString("nombreProducto") == producto.getNombreProducto())
+                        detalleVenta.setProducto(producto);
+                }
+                for(Venta venta:ventaData.listarVentas()){
+                    
+                    if(rs.getInt("idVenta") == venta.getIdVenta())
+                        detalleVenta.setVenta(venta);
+                    if(rs.getDate("fechaVenta").toLocalDate() == venta.getFechaVenta())
+                        detalleVenta.setVenta(venta);
+                }
+                
+                listaDetalleVenta.add(detalleVenta);
             }
+            ps.close();
             
         }catch(SQLException ex){
             
